5c37cb3671a53245643b7e69227b08c037e4bb0c
timtressel <timres@gmx.net>
2019-02-12 10:03:23 +0100

    init: backend

---
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/FrentlyBackendApplication.java
@@ -0,0 +1,14 @@
+package hhu.propra2.illegalskillsexception.frently.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrentlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrentlyBackendApplication.class, args);
+	}
+
+}
+
---
+++ backend/src/test/java/hhu/propra2/illegalskillsexception/frently/backend/FrentlyBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package hhu.propra2.illegalskillsexception.frently.backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrentlyBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
