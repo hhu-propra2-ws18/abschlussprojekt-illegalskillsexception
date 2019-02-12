cff3873ac8edf8795b6332d1358f12d6367a4f3a
Marcel Wagner <marcel.alex.wagner@gmail.com>
2019-02-12 15:05:02 +0100

    feat: "Login component,fixed icon import"

--- frontend/src/View/App/App.js
+++ frontend/src/View/App/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import { Theme as UWPThemeProvider, getTheme } from "react-uwp/Theme";
-import Login from './components/LoginComponent/Login';
-
+import Login from "./components/LoginComponent/Login";
 
 export class App extends Component {
-  render() {
-    return (
-        <UWPThemeProvider
-            theme={getTheme({
-              themeName: "light", // set custom theme
-              accent: "#0078D7", // set accent color
-              useFluentDesign: true, // sure you want use new fluent design.
-            })}
-        >
-          <Login />
-        </UWPThemeProvider>
-    )
-  }
+    render() {
+        return (
+            <UWPThemeProvider
+                theme={getTheme({
+                    themeName: "light", // set custom theme
+                    accent: "#0078D7", // set accent color
+                    useFluentDesign: true // sure you want use new fluent design.
+                })}
+            >
+                <Login />
+            </UWPThemeProvider>
+        );
+    }
 }
 
 export default App;
