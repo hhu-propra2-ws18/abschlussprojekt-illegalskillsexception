f2bc0e98839f607d96a062154bbbea719ec0e4d2
Maximilian Massing <maximilian.massing@googlemail.com>
2019-02-12 12:22:57 +0100

    feat: introduce FrentlyResponse object as return type for REST-Controllers

---
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controllers/Response/FrentlyData.java
@@ -0,0 +1,4 @@
+package hhu.propra2.illegalskillsexception.frently.backend.Controllers.Response;
+
+class FrentlyData {
+}
---
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controllers/Response/FrentlyResponse.java
@@ -0,0 +1,6 @@
+package hhu.propra2.illegalskillsexception.frently.backend.Controllers.Response;
+
+public class FrentlyResponse {
+    public FrentlyError error;
+    public FrentlyData data;
+}
