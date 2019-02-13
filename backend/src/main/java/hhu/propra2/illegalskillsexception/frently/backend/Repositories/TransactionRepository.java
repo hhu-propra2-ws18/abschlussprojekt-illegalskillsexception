257348044958483ba42c32e6eace4de0a69703f1
Weilando <Hendryk.Weiland@hhu.de>
2019-02-13 14:25:25 +0100

    refactor: remove redundant method findTransactionById from TransactionRepository

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Repositories/TransactionRepository.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Repositories/TransactionRepository.java
@@ -7,5 +7,4 @@ import java.util.List;
 
 public interface TransactionRepository extends CrudRepository <Transaction, Long> {
     List<Transaction> findAll();
-    Transaction findTransactionById(Long pID);
 }
