b833c789adf1a7f85a9f893fa13b84c8172638ef
FlMai <f.mai@protonmail.com>
2019-02-13 13:04:52 +0100

    add: add createTransaction method

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Services/TransactionService.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Services/TransactionService.java
@@ -1,8 +1,13 @@
 package hhu.propra2.illegalskillsexception.frently.backend.Services;
 
+import hhu.propra2.illegalskillsexception.frently.backend.Models.Inquiry;
+import hhu.propra2.illegalskillsexception.frently.backend.Models.Transaction;
 import hhu.propra2.illegalskillsexception.frently.backend.Repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 public class TransactionService implements ITransactionService {
@@ -12,5 +17,19 @@ public class TransactionService implements ITransactionService {
     @Autowired
     public TransactionService(TransactionRepository pTransactionRepo) {
         this.transactionRepository = pTransactionRepo;
+    }
+
+    public void createTransaction(Transaction.State state, Inquiry inquiry, LocalDateTime returnDate){
+        final Transaction temp = setTransaction(new Transaction(), inquiry,Transaction.State.open, returnDate);
+        transactionRepository.save(temp);
+    }
+
+    private Transaction setTransaction(Transaction temp, Inquiry inquiry, Transaction.State state, LocalDateTime returnDate) {
+        temp.setInquiry(inquiry);
+        temp.setState(state);
+        temp.setReturnDate(returnDate);
+        temp.setTimestamp(LocalDateTime.now());
+        temp.setUpdated(LocalDateTime.now());
+        return temp;
     }
 }
