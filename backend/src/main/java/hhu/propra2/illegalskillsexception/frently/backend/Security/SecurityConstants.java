200a9dfcdf79ba4c9856e0fe53ac92b0a9914b53
timtressel <timres@gmx.net>
2019-02-13 13:18:41 +0100

    feat: landing page is not secured

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Security/SecurityConstants.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Security/SecurityConstants.java
@@ -6,4 +6,5 @@ class SecurityConstants {
     static final String TOKEN_PREFIX = "Bearer ";
     static final String HEADER_STRING = "Authorization";
     static final String SIGN_UP_URL = "/users/sign-up";
+    static final String LANDING_URL = "/";
 }
