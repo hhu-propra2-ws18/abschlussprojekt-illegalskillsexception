15cdc41ab9632b6b2cd46e21f524d948451452f3
davrad <david@darad.net>
2019-02-13 11:29:21 +0100

    refactor: delete annotation

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controllers/Response/FrentlyData.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controllers/Response/FrentlyData.java
@@ -2,8 +2,6 @@ package hhu.propra2.illegalskillsexception.frently.backend.Controllers.Response;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
 
-@Data
 public interface FrentlyData {
 }
