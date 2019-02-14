e4fd0f3da30be783b96c9ff19829324af442fc2f
Maximilian Massing <maximilian.massing@googlemail.com>
2019-02-14 12:06:33 +0100

    fix: open login path for unauthenticated access

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Security/SecurityConstants.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Security/SecurityConstants.java
@@ -6,5 +6,6 @@ class SecurityConstants {
     static final String TOKEN_PREFIX = "Bearer ";
     static final String HEADER_STRING = "Authorization";
     static final String SIGN_UP_URL = "/users/sign-up";
+    static final String LOGIN_URL = "/login";
     static final String LANDING_URL = "/";
 }
--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Security/WebSecurity.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Security/WebSecurity.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static hhu.propra2.illegalskillsexception.frently.backend.Security.SecurityConstants.LANDING_URL;
+import static hhu.propra2.illegalskillsexception.frently.backend.Security.SecurityConstants.LOGIN_URL;
 import static hhu.propra2.illegalskillsexception.frently.backend.Security.SecurityConstants.SIGN_UP_URL;
 
 
@@ -31,6 +32,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.POST, LOGIN_URL).permitAll()
                 .antMatchers(HttpMethod.GET, LANDING_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
