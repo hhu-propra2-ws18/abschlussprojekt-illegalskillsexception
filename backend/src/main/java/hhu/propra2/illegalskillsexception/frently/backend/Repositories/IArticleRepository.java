02c04114c63dd3692a3be53bd5b0f32a7efebd7c
Julian <J.B.Bokelmann@gmail.com>
2019-02-14 12:03:50 +0100

    add: CRUD methods to ArticleService

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Repositories/IArticleRepository.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Repositories/IArticleRepository.java
@@ -8,5 +8,4 @@ import java.util.List;
 public interface IArticleRepository extends CrudRepository<Article, Long> {
     List<Article> findAll();
     List<Article> findAllByOwner_Id(Long ownerId);
-
 }
