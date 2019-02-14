95c0600a0e858073bbb72217d7f574265ebf0c9e
Weilando <Hendryk.Weiland@hhu.de>
2019-02-14 15:06:04 +0100

    refactor: alter startLend, endLend from LocalDateTime to LocalDate

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Models/LendingPeriod.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Models/LendingPeriod.java
@@ -3,11 +3,16 @@ package hhu.propra2.illegalskillsexception.frently.backend.Models;
 import lombok.Data;
 
 import javax.persistence.Embeddable;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 @Embeddable
 public class LendingPeriod {
-    private LocalDateTime startLend;
-    private LocalDateTime endLend;
+    private LocalDate startLend;
+    private LocalDate endLend;
+
+    public LendingPeriod(LocalDate startLend, LocalDate endLend) {
+        this.startLend = startLend;
+        this.endLend = endLend;
+    }
 }
