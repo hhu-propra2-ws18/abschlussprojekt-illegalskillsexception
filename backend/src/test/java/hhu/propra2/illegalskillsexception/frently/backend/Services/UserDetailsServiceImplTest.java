24a412c219df503b7ae93a678a94da726a0522e1
FlMai <f.mai@protonmail.com>
2019-02-18 15:18:50 +0100

    add: write test for UserDetailsServiceImpl

---
+++ backend/src/test/java/hhu/propra2/illegalskillsexception/frently/backend/Services/UserDetailsServiceImplTest.java
@@ -0,0 +1,34 @@
+package hhu.propra2.illegalskillsexception.frently.backend.Services;
+
+import hhu.propra2.illegalskillsexception.frently.backend.Models.ApplicationUser;
+import hhu.propra2.illegalskillsexception.frently.backend.Repositories.IApplicationUserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static org.mockito.Mockito.*;
+
+public class UserDetailsServiceImplTest {
+
+    private IApplicationUserRepository userRepository;
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Before
+    public void setUp() {
+        userRepository = mock(IApplicationUserRepository.class);
+        when(userRepository.findByUsername("Test")).thenReturn(null);
+        when(userRepository.findByUsername("NoName")).thenReturn(new ApplicationUser());
+        userDetailsService = new UserDetailsServiceImpl(userRepository);
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void loadInvalidUserByUsername() {
+        userDetailsService.loadUserByUsername("Test");
+        verify(userRepository.findByUsername("Test"));
+    }
+
+    @Test
+    public void loadValidUserByUsername() {
+        //TODO: Fill this test
+    }
+}
