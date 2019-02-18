5ffaf5780c44ed67aad8ae91b33f39806f735c16
FlMai <f.mai@protonmail.com>
2019-02-18 13:40:50 +0100

    ref: change createInquiry to return id of new inquiry, includes change to interface

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Services/InquiryService.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Services/InquiryService.java
@@ -18,10 +18,11 @@ public class InquiryService implements IInquiryService{
         this.inquiryRepository = pInquiryRepository;
     }
 
-    public void createInquiry(Article article, ApplicationUser borrower, ApplicationUser lender,
+    public long createInquiry(Article article, ApplicationUser borrower,
                               LendingPeriod lendingPeriod, Inquiry.Status status) {
-        final Inquiry temp = setInquiry(new Inquiry(), article, borrower, lender, lendingPeriod, status);
+        final Inquiry temp = setInquiry(new Inquiry(), article, borrower, article.getOwner(), lendingPeriod, status);
         inquiryRepository.save(temp);
+        return temp.getId();
     }
 
     public Inquiry updateInquiry(Inquiry inquiry) {
