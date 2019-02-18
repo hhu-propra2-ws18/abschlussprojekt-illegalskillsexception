789b09b181a14b5bc19d8044a06e02d28812df9f
sm3sher <sm3sher@gmail.com>
2019-02-18 11:37:07 +0100

    ref: FrentlyResponse assembly

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controllers/Response/FrentlyResponse.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controllers/Response/FrentlyResponse.java
@@ -1,16 +1,11 @@
 package hhu.propra2.illegalskillsexception.frently.backend.Controllers.Response;
 
 import lombok.Data;
-
-import java.util.List;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class FrentlyResponse {
     private FrentlyError error;
-    private List<FrentlyData> frentlyDataList;
-
-    public FrentlyResponse(FrentlyError inputError, List<FrentlyData> inputData) {
-        this.error = inputError;
-        this.frentlyDataList = inputData;
-    }
+    private Object data;
 }
