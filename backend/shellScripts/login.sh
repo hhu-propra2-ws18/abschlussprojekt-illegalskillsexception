3f051d7e7ebfff12b50acc54b909b4d51822decd
Julian <J.B.Bokelmann@gmail.com>
2019-02-19 09:35:28 +0100

    updated: domain.conf

--- backend/shellScripts/login.sh
+++ backend/shellScripts/login.sh
@@ -2,5 +2,5 @@
 curl -i -H "Content-Type: application/json" -X POST -d '{
     "username": "admin",
     "password": "password"
-}' http://localhost:8080/api/login
+}' http://localhost:8080/login
 read  -n 1 -p "" wait
