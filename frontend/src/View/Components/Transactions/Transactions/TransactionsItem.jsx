b08d866643f36eae881dd977de194d0dcd208095
Marcel Wagner <marcel.alex.wagner@gmail.com>
2019-02-19 09:58:45 +0100

    refactor: "Rename process components to transaction components"

--- frontend/src/View/Components/Transactions/Transactions/TransactionsItem.jsx
+++ frontend/src/View/Components/Transactions/Transactions/TransactionsItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class ProcessItem extends React.Component {
+export default class TransactionsItem extends React.Component {
     render() {
         return (
             <article>
