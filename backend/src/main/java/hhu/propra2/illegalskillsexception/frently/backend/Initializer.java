521db1023812664e634a410a0f41c808cfa02ea2
davrad <david@darad.net>
2019-02-19 12:30:23 +0100

    fix: replace randomNumber with randomDouble, cause model change

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Initializer.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Initializer.java
@@ -47,8 +47,8 @@ public class Initializer implements ServletContextInitializer {
         IntStream.range(0, 99).mapToObj(value -> {
             Article a = new Article();
             a.setTitle(faker.commerce().productName());
-            a.setDeposit(faker.number().numberBetween(10, 300));
-            a.setDailyRate(faker.number().numberBetween(1, 100));
+            a.setDeposit(faker.number().randomDouble(2, 10, 300));
+            a.setDailyRate(faker.number().randomDouble(2, 1, 100));
             int randomIndex = faker.number().numberBetween(0, 99);
             a.setOwner(fakeUsers[randomIndex]);
             a.setDescription(faker.lorem().paragraph(3));
