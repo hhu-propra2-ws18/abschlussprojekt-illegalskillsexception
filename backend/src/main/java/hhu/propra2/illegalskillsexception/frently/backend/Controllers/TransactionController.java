cdd11f4378db451a2e4991abc426ea514b18383d
FlMai <f.mai@protonmail.com>
2019-02-19 11:34:35 +0100

    add: add new methods used in controller

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controllers/TransactionController.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controllers/TransactionController.java
@@ -1,7 +1,39 @@
 package hhu.propra2.illegalskillsexception.frently.backend.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import hhu.propra2.illegalskillsexception.frently.backend.Controllers.Response.FrentlyError;
+import hhu.propra2.illegalskillsexception.frently.backend.Controllers.Response.FrentlyErrorType;
+import hhu.propra2.illegalskillsexception.frently.backend.Controllers.Response.FrentlyResponse;
+import hhu.propra2.illegalskillsexception.frently.backend.Models.ApplicationUser;
+import hhu.propra2.illegalskillsexception.frently.backend.Services.ApplicationUserService;
+import hhu.propra2.illegalskillsexception.frently.backend.Services.TransactionService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/transaction")
+@AllArgsConstructor
 public class TransactionController {
+
+    private TransactionService transactionService;
+    private ApplicationUserService userService;
+
+    @GetMapping("/getAll")
+    public FrentlyResponse getAllTransactionsForOwner(Authentication authentication) {
+        FrentlyResponse response = new FrentlyResponse();
+        ApplicationUser user = userService.getCurrentUser(authentication);
+        try {
+            response.setData(transactionService.exportTransactions(user.getId()));
+        } catch (Exception e) {
+            FrentlyError error = new FrentlyError("Could not extract transactions from user", FrentlyErrorType.MISC);
+            response.setError(error);
+        }
+        return response;
+    }
+
+    @PostMapping("/itemBorrowerReturn")
+    public FrentlyResponse returnArticle(Authentication authentication, @RequestParam long id) {
+        //TODO
+        return new FrentlyResponse();
+    }
 }
