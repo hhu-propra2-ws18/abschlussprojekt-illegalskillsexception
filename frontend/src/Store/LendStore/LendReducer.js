049acbf8b143c7693c3b6527cc39f0a461aaace4
Marcel Wagner <marcel.alex.wagner@gmail.com>
2019-02-19 10:42:02 +0100

    add: "Add update lend item functionality"

--- frontend/src/Store/LendStore/LendReducer.js
+++ frontend/src/Store/LendStore/LendReducer.js
@@ -1,12 +1,21 @@
-import { ADD_LEND_ITEM, SET_LEND_ITEMS } from "./LendActions";
+import { ADD_LEND_ITEM, SET_LEND_ITEMS, UPDATE_LEND_ITEM } from "./LendActions";
 
 export default function lendstore(state = [], action) {
     switch (action.type) {
         case ADD_LEND_ITEM: {
-            return [...state,action.data];
+            return [...state, action.data];
         }
-        case SET_LEND_ITEMS:{
+        case SET_LEND_ITEMS: {
             return action["list"];
+        }
+        case UPDATE_LEND_ITEM: {
+            let copy = Object.assign([], state);
+            copy.forEach(item => {
+                if (item.id === action.data.id) {
+                    Object.assign(item, action.data);
+                }
+            });
+            return copy;
         }
         default: {
             return [];
