a424575320fd7d0cfd5d8632af9a1ad8197b993d
Weilando <Hendryk.Weiland@hhu.de>
2019-02-19 11:18:57 +0100

    feat: add ProPayAccount and ProPayReservation to handle ProPay's account information

---
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Models/ProPayReservation.java
@@ -0,0 +1,9 @@
+package hhu.propra2.illegalskillsexception.frently.backend.Models;
+
+import lombok.Data;
+
+@Data
+public class ProPayReservation {
+    private Long id;
+    private Double amount;
+}
