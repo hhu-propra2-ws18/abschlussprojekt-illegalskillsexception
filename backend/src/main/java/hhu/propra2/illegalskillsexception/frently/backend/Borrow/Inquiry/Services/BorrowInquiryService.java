e5448c565e910e326961da22f45052deda921edb
Maximilian Massing <maximilian.massing@googlemail.com>
2019-02-21 12:58:57 +0100

    refactor: move line

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Borrow/Inquiry/Services/BorrowInquiryService.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Borrow/Inquiry/Services/BorrowInquiryService.java
@@ -31,6 +31,8 @@ public class BorrowInquiryService implements IBorrowInquiryService {
         ApplicationUser currentUser = userService.getCurrentUser(auth);
 
         if (isInvalidPeriod(dto)) throw new InvalidLendingPeriodException();
+        if (hasDateConflict(dto)) throw new ArticleNotAvailableException();
+
         Inquiry inquiry = new Inquiry();
 
         Article article = articleService.getArticleById(dto.getArticleId());
@@ -42,7 +44,6 @@ public class BorrowInquiryService implements IBorrowInquiryService {
         inquiry.setStartDate(dto.getStartDate());
         inquiry.setEndDate(dto.getEndDate());
 
-        if (hasDateConflict(dto)) throw new ArticleNotAvailableException();
 
         inquiries.save(inquiry);
         return inquiry;
