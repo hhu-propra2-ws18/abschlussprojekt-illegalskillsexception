88a01e85a860dcb2da36aae7cf50aeddd7d8ae4a
Marcel Wagner <marcel.alex.wagner@gmail.com>
2019-02-22 13:52:00 +0100

    feat: "Update urls for transacionts, fix inquiry accept url"

--- frontend/src/Services/urlConstants.js
+++ frontend/src/Services/urlConstants.js
@@ -21,28 +21,9 @@ export const LEND_UPDATE = `${BASEURL}${LEND}article/update`;
 
 
 export const INQUIRY_GETALL_LEND = `${BASEURL}${LEND}inquiry/`;
-export const INQUIRY_ACCEPT = `${BASEURL}inquiry/accept`;
+export const INQUIRY_ACCEPT = `${BASEURL}${LEND}inquiry/accept`;
 export const INQUIRY_DECLINE = `${BASEURL}${LEND}inquiry/decline`;
 
-export const TRANSACTION_GETALL = `${BASEURL}transaction/getAll`;
-export const TRANSACTION_PROBLEM = `${BASEURL}transaction/problem`;
-
-/*
-
-export const BASEURL = "http://localhost:8080/api/";
-
-export const USER_PROFILE = `${BASEURL}${USER}/`;
-export const SIGN_UP = `${BASEURL}${USER}/sign-up`;
-export const LOGINURL = `http://localhost:8080/login`;
-
-export const INQUIRY_GETALL_BORROW = `${BASEURL}${BORROW}inquiry/`;
-
-export const TRANSACTION_GETALL = `${BASEURL}${BORROW}transaction/`;
-
-export const INQUIRY_ACCEPT = `${BASEURL}${LEND}inquiry/accept`;
-
-export const TRANSACTION_PROBLEM = `${BASEURL}${LEND}transaction/problem`;
-
-export const TRANSACTION_RETURNED = `${BASEURL}${LEND}transaction/update`;
-
-*/
+export const TRANSACTION_GETALL = `${BASEURL}${LEND}transaction/`;
+export const TRANSACTION_FINISHED = `${BASEURL}${LEND}transaction/update`
+export const TRANSACTION_PROBLEM = `${BASEURL}${LEND}transaction/update`;
