301b1101a3de399728dcf46dd82a0fd4390dc1f9
FlMai <f.mai@protonmail.com>
2019-02-22 15:11:19 +0100

    add: new /notification rout in user

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Data/Repositories/ITransactionRepository.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Data/Repositories/ITransactionRepository.java
@@ -3,10 +3,13 @@ package hhu.propra2.illegalskillsexception.frently.backend.Data.Repositories;
 import hhu.propra2.illegalskillsexception.frently.backend.Data.Models.Transaction;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ITransactionRepository extends CrudRepository <Transaction, Long> {
     List<Transaction> findAll();
 
     List<Transaction> findAllByInquiry_Borrower_Id(long id);
+
+    List<Transaction> findAllByInquiry_Borrower_IdAndInquiry_EndDateGreaterThan(long userId, LocalDate nowDate);
 }
