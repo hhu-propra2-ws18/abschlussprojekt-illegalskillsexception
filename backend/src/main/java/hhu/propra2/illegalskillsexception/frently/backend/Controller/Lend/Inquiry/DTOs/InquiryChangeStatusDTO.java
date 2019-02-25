ad2679b7436aef16d00a15fba5482d913eaad0cd
Maximilian Massing <maximilian.massing@googlemail.com>
2019-02-25 09:52:42 +0100

    fix: InquiryChangeStatusDTO needs setter

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controller/Lend/Inquiry/DTOs/InquiryChangeStatusDTO.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controller/Lend/Inquiry/DTOs/InquiryChangeStatusDTO.java
@@ -1,9 +1,8 @@
 package hhu.propra2.illegalskillsexception.frently.backend.Controller.Lend.Inquiry.DTOs;
 
+import lombok.Data;
+
+@Data
 public class InquiryChangeStatusDTO {
     private long inquiryId;
-
-    public long getInquiryId() {
-        return inquiryId;
-    }
 }
