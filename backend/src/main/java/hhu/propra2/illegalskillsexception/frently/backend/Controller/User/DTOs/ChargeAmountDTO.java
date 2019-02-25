d1f4d2ee4f9de5ff5d84bd611c6f88778465b575
Maximilian Massing <maximilian.massing@googlemail.com>
2019-02-25 09:49:44 +0100

    fix: ChargeAmountDTO needs setter

--- backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controller/User/DTOs/ChargeAmountDTO.java
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Controller/User/DTOs/ChargeAmountDTO.java
@@ -1,9 +1,8 @@
 package hhu.propra2.illegalskillsexception.frently.backend.Controller.User.DTOs;
 
+import lombok.Data;
+
+@Data
 public class ChargeAmountDTO {
     private Long amount;
-
-    public Long getAmount() {
-        return amount;
-    }
 }
