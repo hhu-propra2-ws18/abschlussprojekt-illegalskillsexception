37cee618b393d1e8a152fb418c4aa96cd1d5134b
timtressel <timres@gmx.net>
2019-02-25 15:07:21 +0100

    feat: file :cat:

---
+++ backend/src/main/java/hhu/propra2/illegalskillsexception/frently/backend/Data/Repositories/IRoleRepository.java
@@ -0,0 +1,8 @@
+package hhu.propra2.illegalskillsexception.frently.backend.Data.Repositories;
+
+import hhu.propra2.illegalskillsexception.frently.backend.Data.Models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IRoleRepository extends CrudRepository<Role, Long> {
+
+}
