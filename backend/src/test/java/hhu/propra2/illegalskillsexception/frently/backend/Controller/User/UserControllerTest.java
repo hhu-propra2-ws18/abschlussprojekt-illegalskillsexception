6488ac726ecaf6dae8f2b5fcc0a33dbeb4b1cf40
Julian <J.B.Bokelmann@gmail.com>
2019-02-28 14:55:30 +0100

    test: UserController add tests

--- backend/src/test/java/hhu/propra2/illegalskillsexception/frently/backend/Controller/User/UserControllerTest.java
+++ backend/src/test/java/hhu/propra2/illegalskillsexception/frently/backend/Controller/User/UserControllerTest.java
@@ -1,14 +1,16 @@
 package hhu.propra2.illegalskillsexception.frently.backend.Controller.User;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hhu.propra2.illegalskillsexception.frently.backend.Controller.MockAuthentication;
 import hhu.propra2.illegalskillsexception.frently.backend.Controller.Security.ApplicationUserDTO;
-import hhu.propra2.illegalskillsexception.frently.backend.Controller.User.DTOs.MoneyTransferDTO;
-import hhu.propra2.illegalskillsexception.frently.backend.Controller.User.DTOs.UserDetailResponse;
+import hhu.propra2.illegalskillsexception.frently.backend.Controller.User.DTOs.*;
 import hhu.propra2.illegalskillsexception.frently.backend.Controller.User.Exceptions.UserAlreadyExistsAuthenticationException;
 import hhu.propra2.illegalskillsexception.frently.backend.Controller.User.IServices.IApplicationUserService;
 import hhu.propra2.illegalskillsexception.frently.backend.Controller.User.IServices.IUserDetailService;
 import hhu.propra2.illegalskillsexception.frently.backend.Controller.User.IServices.IUserTransactionService;
 import hhu.propra2.illegalskillsexception.frently.backend.Controller.User.Services.SecurityUserService;
+import hhu.propra2.illegalskillsexception.frently.backend.Data.Models.ApplicationUser;
+import hhu.propra2.illegalskillsexception.frently.backend.Data.Models.Transaction;
 import hhu.propra2.illegalskillsexception.frently.backend.ProPay.Exceptions.ProPayConnectionException;
 import hhu.propra2.illegalskillsexception.frently.backend.ProPay.IServices.IProPayService;
 import org.hamcrest.core.IsNull;
@@ -16,16 +18,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.retry.ExhaustedRetryException;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.verify;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
@@ -36,7 +39,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 public class UserControllerTest {
-
     @InjectMocks
     UserController userController;
     private MockMvc mockMvc;
@@ -100,6 +102,8 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.error.errorType").value("PROPAY_CONNECTION"))
                 .andExpect(jsonPath("$.data").value(IsNull.nullValue()));
 
+
+        verify(mockUserService, times(0)).createUser(any());
         verify(mockProPayService, times(1)).createAccount(any(), eq(0.0));
     }
 
@@ -123,10 +127,10 @@ public class UserControllerTest {
 
 
         verify(mockUserService, times(1)).createUser(any());
+        verify(mockProPayService, times(1)).createAccount(any(), eq(0.0));
     }
 
     @Test
-    @WithMockUser(username = "Hannes", password = "123")
     public void getHannesUserDetailsSuccessfully() throws Exception {
         UserDetailResponse hannesUserDetails = new UserDetailResponse();
         hannesUserDetails.setAccountBalance(300.0);
@@ -162,7 +166,6 @@ public class UserControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "Hannes", password = "123")
     public void getHannesUserDetailsFailsBecauseProPayDoesNotAnswer() throws Exception {
         when(mockUserDetailService.getUserDetails(any())).thenThrow(ProPayConnectionException.class);
 
@@ -175,4 +178,162 @@ public class UserControllerTest {
 
         verify(mockUserDetailService, times(1)).getUserDetails(any());
     }
+
+    @Test
+    public void getHannesUserDetailsWithoutBeingHannes() throws Exception {
+
+        ForeignUserDetailRequest foreignUserDetailRequest = new ForeignUserDetailRequest();
+        foreignUserDetailRequest.setUsername("Hannes");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(foreignUserDetailRequest);
+
+        MoneyTransferDTO transferDTO = new MoneyTransferDTO();
+        transferDTO.setReceiver("Hannes");
+        transferDTO.setSender("Max");
+        transferDTO.setAmount(300.0);
+        ArrayList<MoneyTransferDTO> moneyTransferDTOList = new ArrayList<>();
+        moneyTransferDTOList.add(transferDTO);
+
+        ForeignUserDetailResponse foreignUserDetailResponse = new ForeignUserDetailResponse();
+        foreignUserDetailResponse.setUsername("Hannes");
+        foreignUserDetailResponse.setCompletedTransactions(moneyTransferDTOList);
+
+        when(mockUserDetailService.getForeignUserDetails(any())).thenReturn(foreignUserDetailResponse);
+
+        this.mockMvc.perform(post("/user/").contentType(APPLICATION_JSON_UTF8)
+                .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.error").value(IsNull.nullValue()))
+                .andExpect(jsonPath("$.data.username").value("Hannes"))
+                .andExpect(jsonPath("$.data.completedTransactions[0].sender").value("Max"))
+                .andExpect(jsonPath("$.data.completedTransactions[0].receiver").value("Hannes"))
+                .andExpect(jsonPath("$.data.completedTransactions[0].amount").value("300.0"));
+
+        verify(mockUserDetailService, times(1)).getForeignUserDetails(any());
+    }
+
+    @Test
+    public void getHannesUserDetailsWithoutBeingHannesFailsBecauseProPayDoesNotAnswer() throws Exception {
+
+        ForeignUserDetailRequest foreignUserDetailRequest = new ForeignUserDetailRequest();
+        foreignUserDetailRequest.setUsername("Hannes");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(foreignUserDetailRequest);
+
+        MoneyTransferDTO transferDTO = new MoneyTransferDTO();
+        transferDTO.setReceiver("Hannes");
+        transferDTO.setSender("Max");
+        transferDTO.setAmount(300.0);
+        ArrayList<MoneyTransferDTO> moneyTransferDTOList = new ArrayList<>();
+        moneyTransferDTOList.add(transferDTO);
+
+        ForeignUserDetailResponse foreignUserDetailResponse = new ForeignUserDetailResponse();
+        foreignUserDetailResponse.setUsername("Hannes");
+        foreignUserDetailResponse.setCompletedTransactions(moneyTransferDTOList);
+
+        when(mockUserDetailService.getForeignUserDetails(any())).thenThrow(ProPayConnectionException.class);
+
+        this.mockMvc.perform(post("/user/").contentType(APPLICATION_JSON_UTF8)
+                .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.error.errorType").value("PROPAY_CONNECTION"))
+                .andExpect(jsonPath("$.data").value(IsNull.nullValue()));
+
+        verify(mockUserDetailService, times(1)).getForeignUserDetails(any());
+    }
+
+    @Test
+    public void chargeUpHannesAccountWith300Euro() throws Exception {
+        MockAuthentication mockAuthentication = new MockAuthentication();
+        mockAuthentication.setPrinciple("Hannes");
+        ChargeAmountDTO chargeAmountDTO = new ChargeAmountDTO();
+        chargeAmountDTO.setAmount(300L);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(chargeAmountDTO);
+
+        this.mockMvc.perform(post("/user/charge").contentType(APPLICATION_JSON_UTF8)
+                .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.error").value(IsNull.nullValue()))
+                .andExpect(jsonPath("$.data").value(IsNull.nullValue()));
+
+        verify(mockProPayService, times(1)).payInMoney(any(), eq(300.0));
+    }
+
+    @Test
+    public void chargeUpHannesAccountWith300EuroFailsBecauseProPayDoesNotRespond() throws Exception {
+        MockAuthentication mockAuthentication = new MockAuthentication();
+        mockAuthentication.setPrinciple("Hannes");
+        ChargeAmountDTO chargeAmountDTO = new ChargeAmountDTO();
+        chargeAmountDTO.setAmount(300L);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(chargeAmountDTO);
+
+        doThrow(ExhaustedRetryException.class).when(mockProPayService).payInMoney(any(), eq(300.0));
+
+        this.mockMvc.perform(post("/user/charge").contentType(APPLICATION_JSON_UTF8)
+                .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.error.errorType").value("PROPAY_CONNECTION"))
+                .andExpect(jsonPath("$.data").value(IsNull.nullValue()));
+
+        verify(mockProPayService, times(1)).payInMoney(any(), eq(300.0));
+    }
+
+    @Test
+    public void notificationsOneOverdueTransaction() throws Exception {
+        ApplicationUser applicationUser = new ApplicationUser();
+        applicationUser.setUsername("Hans");
+        applicationUser.setEmail("HansSupercool@yahoo.com");
+        applicationUser.setPassword("123");
+        applicationUser.setId(1);
+
+        Transaction transaction = Mockito.mock(Transaction.class);
+        ArrayList<Transaction> transactionArrayList = new ArrayList<>();
+        transactionArrayList.add(transaction);
+
+        when(mockUserService.getCurrentUser(any())).thenReturn(applicationUser);
+        when(mockTransactionService.allOverdueTransactions(eq(1))).thenReturn(transactionArrayList);
+
+        this.mockMvc.perform(get("/user/notifications").contentType(APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.error").value(IsNull.nullValue()))
+                .andExpect(jsonPath("$.data", hasSize(0)));
+    }
+
+    @Test
+    public void notificationsNoOverdueTransactions() throws Exception {
+        ApplicationUser applicationUser = new ApplicationUser();
+        applicationUser.setUsername("Hans");
+        applicationUser.setEmail("HansSupercool@yahoo.com");
+        applicationUser.setPassword("123");
+        applicationUser.setId(1);
+
+        Transaction transaction = Mockito.mock(Transaction.class);
+        ArrayList<Transaction> transactionArrayList = new ArrayList<>();
+        transactionArrayList.add(transaction);
+
+        when(mockTransactionService.allOverdueTransactions(eq(1))).thenReturn(null);
+
+        this.mockMvc.perform(get("/user/notifications").contentType(APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.error.errorType").value("NO_SUCH_TRANSACTION"))
+                .andExpect(jsonPath("$.data").value(IsNull.nullValue()));
+
+    }
+
 }
