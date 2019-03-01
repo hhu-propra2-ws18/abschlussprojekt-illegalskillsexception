e5753894a0f05af6a2a39857f112a49f40b94720
Weilando <Hendryk.Weiland@hhu.de>
2019-03-01 14:48:40 +0100

    test: test loading a valid user

--- backend/src/test/java/hhu/propra2/illegalskillsexception/frently/backend/Controller/Services/SecurityUserServiceTest.java
+++ backend/src/test/java/hhu/propra2/illegalskillsexception/frently/backend/Controller/Services/SecurityUserServiceTest.java
@@ -2,11 +2,14 @@ package hhu.propra2.illegalskillsexception.frently.backend.Controller.Services;
 
 import hhu.propra2.illegalskillsexception.frently.backend.Controller.User.Services.SecurityUserService;
 import hhu.propra2.illegalskillsexception.frently.backend.Data.Models.ApplicationUser;
+import hhu.propra2.illegalskillsexception.frently.backend.Data.Models.Role;
 import hhu.propra2.illegalskillsexception.frently.backend.Data.Repositories.IApplicationUserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.test.context.support.WithMockUser;
 
+import java.util.HashSet;
 import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
@@ -20,19 +23,33 @@ public class SecurityUserServiceTest {
     @Before
     public void setUp() {
         userRepository = mock(IApplicationUserRepository.class);
-        when(userRepository.findByUsername("Test")).thenReturn(Optional.empty());
-        when(userRepository.findByUsername("NoName")).thenReturn(Optional.of(new ApplicationUser()));
         userDetailsService = new SecurityUserService(userRepository);
     }
 
     @Test(expected = UsernameNotFoundException.class)
     public void loadInvalidUserByUsername() {
-        userDetailsService.loadUserByUsername("Test");
-        userRepository.findByUsername("Test");
+        when(userRepository.findByUsername("Karl")).thenReturn(Optional.empty());
+        userDetailsService.loadUserByUsername("Karl");
     }
 
     @Test
+    @WithMockUser(username = "Karl")
     public void loadValidUserByUsername() {
-        //TODO: Fill this test
+        Role userRole = new Role();
+        userRole.setId(0L);
+        userRole.setName("USER");
+        userRole.setDescription("Basic user");
+
+        HashSet<Role> roles = new HashSet<>();
+        roles.add(userRole);
+
+        ApplicationUser user = new ApplicationUser();
+        user.setUsername("Karl");
+        user.setPassword("password");
+        user.setRoles(roles);
+
+        when(userRepository.findByUsername("Karl")).thenReturn(Optional.of(user));
+
+        userDetailsService.loadUserByUsername("Karl");
     }
 }
