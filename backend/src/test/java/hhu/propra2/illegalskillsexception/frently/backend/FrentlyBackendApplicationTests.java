c62fdad56f047174ddcb937946f42dfa2856266c
Weilando <Hendryk.Weiland@hhu.de>
2019-03-01 15:01:57 +0100

    test: fill test

--- backend/src/test/java/hhu/propra2/illegalskillsexception/frently/backend/FrentlyBackendApplicationTests.java
+++ backend/src/test/java/hhu/propra2/illegalskillsexception/frently/backend/FrentlyBackendApplicationTests.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +23,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.mockito.Mockito.mock;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -53,7 +55,9 @@ public class FrentlyBackendApplicationTests {
     }
 
     @Test
-    public void contextLoads() {
+    @WithMockUser
+    public void contextLoads() throws Exception {
+        this.mockMvc.perform(get("/")).andExpect(status().isOk());
     }
 
     @Test
