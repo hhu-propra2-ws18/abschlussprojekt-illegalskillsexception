ee37beeee5d2020a86a84a3126fb9bac45b5a25b
Marcel Wagner <marcel.alex.wagner@gmail.com>
2019-03-01 12:11:46 +0100

    feat: "Improve UI; add magnificent rainbow"

--- frontend/src/View/App/Header/Header.jsx
+++ frontend/src/View/App/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
 
 export default class Header extends React.Component {
     render() {
-        return <header>
-            <h1><span>f</span>RENT<span>ly</span></h1>
-        </header>;
+        return (
+            <header>
+                <h1>
+                    <div>
+                        <span>f</span>RENT<span>ly</span>
+                    </div>
+                </h1>
+            </header>
+        );
     }
 }
--- frontend/src/View/Components/ContentTabs/Consumer/Buy/BuyItemComponent/BuyItemComponent.jsx
+++ frontend/src/View/Components/ContentTabs/Consumer/Buy/BuyItemComponent/BuyItemComponent.jsx
@@ -3,6 +3,8 @@ import Dialog from "react-uwp/Dialog";
 import { buyItem } from "../../../../../../Services/Buy/buyCompleteService";
 import Button from "react-uwp/Button";
 
+import "./BuyItemComponent.css"
+
 export default class BuyItemComponent extends Component {
     constructor(props) {
         super(props);
@@ -34,56 +36,63 @@ export default class BuyItemComponent extends Component {
 
     render() {
         return (
-            <article>
+            <article className="buy-article-container">
+                <div>
                 <h3>{this.props.data.title}</h3>
-                <h5>Description:</h5>
-                <p>{this.props.data.description}</p>
-                <h5>Price:</h5>
-                <p>{this.props.data.price}</p>
-                <h5>Location:</h5>
-                <p>{this.props.data.location}</p>
-                <Button className="bottom-button" onClick={() => this.setState({ showBuyDialog: true })}>
-                    Buy item
-                </Button>
-                {this.state.showBuyDialog ? (
-                    <Dialog
-                        defaultShow={this.state.showBuyDialog}
-                        onCloseDialog={() => this.hideBuyDialog()}
+                    <h5>Description:</h5>
+                    <p>{this.props.data.description}</p>
+                    <h5>Price:</h5>
+                    <p>{this.props.data.price}</p>
+                    <h5>Location:</h5>
+                    <p>{this.props.data.location}</p>
+                    <Button
+                        className="bottom-button"
+                        onClick={() => this.setState({ showBuyDialog: true })}
                     >
-                        <article>
-                            <p>
-                                Are you sure you want to buy this item for{" "}
-                                {this.props.data.price}€ ?
-                            </p>
-                            <div className="dialog-buttons">
-                                <Button
-                                    onClick={() => {
-                                        this.processBuying();
-                                    }}
-                                >
-                                    Yes
+                        Buy item
+                    </Button>
+                    {this.state.showBuyDialog ? (
+                        <Dialog
+                            defaultShow={this.state.showBuyDialog}
+                            onCloseDialog={() => this.hideBuyDialog()}
+                        >
+                            <article>
+                                <p>
+                                    Are you sure you want to buy this item for{" "}
+                                    {this.props.data.price}€ ?
+                                </p>
+                                <div className="dialog-buttons">
+                                    <Button
+                                        onClick={() => {
+                                            this.processBuying();
+                                        }}
+                                    >
+                                        Yes
+                                    </Button>
+                                    <Button
+                                        onClick={() => this.hideBuyDialog()}
+                                    >
+                                        No
+                                    </Button>
+                                </div>
+                            </article>
+                        </Dialog>
+                    ) : null}
+                    {this.state.showErrorDialog ? (
+                        <Dialog
+                            defaultShow={this.state.showErrorDialog}
+                            onClick={() => this.hideErrorDialog()}
+                        >
+                            <article>
+                                <h4>Sorry, something went wrong.</h4>
+                                <p>{this.state.error.errorMessage}</p>
+                                <Button onClick={() => this.hideErrorDialog()}>
+                                    Close
                                 </Button>
-                                <Button onClick={() => this.hideBuyDialog()}>
-                                    No
-                                </Button>
-                            </div>
-                        </article>
-                    </Dialog>
-                ) : null}
-                {this.state.showErrorDialog ? (
-                    <Dialog
-                        defaultShow={this.state.showErrorDialog}
-                        onClick={() => this.hideErrorDialog()}
-                    >
-                        <article>
-                            <h4>Sorry, something went wrong.</h4>
-                            <p>{this.state.error.errorMessage}</p>
-                            <Button onClick={() => this.hideErrorDialog()}>
-                                Close
-                            </Button>
-                        </article>
-                    </Dialog>
-                ) : null}
+                            </article>
+                        </Dialog>
+                    ) : null}
+                </div>
             </article>
         );
     }
